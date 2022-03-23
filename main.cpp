2d08a8b51a2eaa2e733b9dc5ba4ce4d5d2953636
Brian Horner <bhorner@suffolk.edu>
2022-03-22 21:15:03 -0400

    added function to calculate offset, taga nd slot

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 // 2/5/2022 - Initial Version and Establishing Git Repo
 // 3/22/2022 - Added populateMainMem and displayMainMem funcs
 // 3/22/2022 - Fixed populateMainMem func
+// 3/22/2022 - Added calcAddressValues func
 
 // Standard Library Includes
 #include <iostream>
@@ -14,6 +15,8 @@
 
 void populateMainMem(short *, short);
 void displayMainMem(short *, short);
+void calcAddressValues(short);
+
 
 int main() {
 
@@ -21,8 +24,24 @@ int main() {
     populateMainMem(main_mem, 2048);
     displayMainMem(main_mem, 2048);
 
+    short testAddress{0x14A};
+
+    calcAddressValues(testAddress);
+
     return 0;
 }
+
+void calcAddressValues(short address){
+    unsigned short mask = 0x00F;
+    unsigned short offset = mask & address;
+    unsigned short slot = ((mask <<4) & address)>>4;
+    unsigned short tag = ((mask <<8) & address)>>8;
+
+    std::cout << "Offset: " <<std::hex<<std::uppercase <<offset<<std::endl;
+    std::cout << "Slot: " << slot <<std::endl;
+    std::cout << "Tag: " << tag <<std::endl;
+}
+
 
 
 void populateMainMem(short *mainMem, short mainMemSize){
